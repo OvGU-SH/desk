abfe6d735b5526914300c3275e280692421a8b9d
TK-UniTrier <66017659+TK-UniTrier@users.noreply.github.com>
2022-09-16 13:41:08 +0200

    Add files via upload

---
+++ R/zzz.R
@@ -0,0 +1,19 @@
+.onLoad = function(libname, pkgname){
+  # packageStartupMessage("\n\n Loading DESK ... \014")
+  # set custom options for the package
+}
+
+.onAttach = function(libname, pkgname){
+  packageStartupMessage(
+    "\n
+*********************************************************************************
+* DESK is released for EDUCATIONAL PURPOSES, accompanying the german textbooks: *
+*                                                                               *
+* L.v.Auer (2023) \u00d6konometrie - Eine Einf\u00fchrung, 8th ed.                        *
+* L.v.Auer, S.Hoffmann & T.Kranz (2023) \u00d6konometrie - Das R-Arbeitsbuch, 2nd ed.*
+*                                                                               *
+*********************************************************************************
+\n"
+  )
+}
+
